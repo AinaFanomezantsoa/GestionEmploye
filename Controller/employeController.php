50bdcd5919358802b519b904a3b84f9eb5a567ea
Aina Fanomezantsoa <aina.19fanomezantsoa@gmail.com>
2022-11-14 17:54:55 +0300

    modif et recherche

--- Controller/employeController.php
+++ Controller/employeController.php
@@ -23,30 +23,31 @@
                     }
 
                 break;
-        case "modifier":
-            if(isset($_GET['id'])){
-                $id = $_GET['id'];
-                foreach ($db->getData("SELECT * FROM employe WHERE id='$id'") as $row){
-                    $nom_employe = isset($row['nom_employe']) ?$row['nom_employe']:'';
-                    $prenom_employe = isset($row['prenom_employe']) ?$row['prenom_employe']:'';
-                }
-            }
-                # logique...
-                    require_once "View/modif.php";
-                break;
-                case "edit":
+                case "modifier":
+
+                    $id = $_GET['id'];
+                    $dataMod = $db->getData("SELECT * FROM employe WHERE id='$id'");
+                    // foreach ($db->getData("SELECT * FROM employe WHERE id='$id'") as $row){
+                    //     $nom_employe = $row['nom_employe'];
+                    //     $prenom_employe = $row['prenom_employe'];
+                    // }
+    
                     # logique...
-                        $nom_employe = $_POST['nom_employe'];
-                            $prenom_employe = $_POST['prenom_employe'];
-        
-                            if (empty($nom_employe) || empty($prenom_employe)) {
-                                $error = "Not null";
-                                print_r($error);
-                            }else{
-                                $db->action("UPDATE employe SET nom_employe= '$nom_employe', prenom_employe= '$prenom_employe' WHERE id= $id");
-                                header("Location:index.php?Controller=employe");
-                            }
+                    require_once "View/modif.php";
                     break;
+                    case "edit":
+                        # logique...
+                            $nom_employe = $_POST['nom_employe'];
+                                $prenom_employe = $_POST['prenom_employe'];
+                                $id = $_POST['id'];
+                                if (empty($nom_employe) || empty($prenom_employe)) {
+                                    $error = "Not null";
+                                    print_r($error);
+                                }else{
+                                    $db->action("UPDATE employe SET nom_employe= '$nom_employe', prenom_employe= '$prenom_employe' WHERE id= $id");
+                                    header("Location:index.php?Controller=employe");
+                                }
+                        break;
                 //UPDATE `employe` SET `nom_employe` = 'Atory' WHERE `employe`.`id` = 4;
         case "supprimer":
             # logique...
@@ -55,6 +56,21 @@
                 $db->action("DELETE FROM employe WHERE id = '$id' ");
                 header("Location:index.php?Controller=employe");
             }
+            break;
+
+        case "recherche":
+            # logique...
+            @$keywords = $_GET["keywords"];
+            @$valider = $_GET["valider"];
+            if(isset($valider) && !empty(trim($keywords))){
+                $res=$db->getData("SELECT nom_employe FROM employe WHERE nom_employe LIKE '%$keywords%'");
+                $res->setFetchMode(PDO::FETCH_ASSOC);
+                $res->execute();
+                $tab=$res-fetchAll();
+                $afficher="oui";
+                header("Location:index.php?Controller=employe");
+}
+            require_once "View/index.php";
             break;
         default:
             # logique...
--- View/index.php
+++ View/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +7,30 @@
     <title>Gestion Employé</title>
 </head>
 <body>
+<form action="" method="get">
+<input type="search" name="keywords" value="">
+<input type="submit" name= "valider" value="recherche"><br>
+</form>
+<table class="border">
+        <thead>
+            <tr>
+                <td>Nom Employé</td>
+                <td>Prénom Employé</td>
+                <td>Action</td>
+            </tr>
+        </thead>
+        <tbody>
+                    <tr>
+                    <td>Aina</td>
+                    <td>Aina</td>
+                    <td>
+                        <a href="" class="btn">Modifier</a>
+                        <a href="" class="btn">Supprimer</a>
+                    </td>
+                </tr>
+        </tbody>
+    </table>
+    <h1>Liste Employé</h1>
     <a href="index.php?Controller=employe&action=ajout" class="btn">Ajout</a>
     <table class="border">
         <thead>
@@ -20,16 +43,16 @@
         <tbody>
             <?php
                 foreach ($data as $employe){
-                ?>
-                <tr>
-                <td><?=$employe['nom_employe']?></td>
-                <td><?=$employe['prenom_employe']?></td>
-                <td>
-                    <a href="index.php?Controller=employe&action=modifier&id=<?=$employe['id']?>" class="btn">Modifier</a>
-                    <a href="index.php?Controller=employe&action=supprimer&id=<?=$employe['id']?>" class="btn">Supprimer</a>
-                </td>
-            </tr>
-                <?php
+                    ?>
+                    <tr>
+                    <td><?php echo $employe['nom_employe']?></td>
+                    <td><?php echo $employe['prenom_employe']?></td>
+                    <td>
+                        <a href="index.php?Controller=employe&action=modifier&id=<?php echo $employe['id']; ?>" class="btn">Modifier</a>
+                        <a href="index.php?Controller=employe&action=supprimer&id=<?php echo $employe['id']; ?>" class="btn">Supprimer</a>
+                    </td>
+                </tr>
+                    <?php
             }
             ?>
         </tbody>
